f60d3a23a7214de8da4cee8906a6a5cbd3711bba
Cort Harlow <cort.harlow@gmail.com>
2016-02-21 11:27:36 -0500

    Interest page, switching to openings list

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -13,4 +13,40 @@ class InterestsController < ApplicationController
     @interest.user_id = current_user.id
 
     if @interest.save
-      redirect_to new_user_interest_path
+      redirect_to company_openings_path
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @interest = Interest.find(params[:id])
+
+    if @interest.update(interest_params)
+      redirect_to @interest.user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @interest = Interest.find(params[:id])
+    redirect_to @interest.user
+    @itinerary.destroy
+  end
+
+  private
+
+  def interest_params
+    params.require(:interest).permit(
+      :user_id,
+      :technology,
+      :medical,
+      :construction,
+      :create,
+      :culinary,
+      :industrial,
+      :mechanical
+    )
+  end
+end
